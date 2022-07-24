82a85e761ce159f161a5d779712cff1bb8eede12
unknown <bazo20061996@gmail.com>
2022-07-24 22:57:46 +0700

    MD2-bai1-thoi gian may tinh

---
+++ src/HienThiTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class HienThiTime {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("bây giờ là " + date);
+    }
+}
